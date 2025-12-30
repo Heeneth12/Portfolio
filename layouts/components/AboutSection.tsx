64ee422130264e09c17f1885d21364d708f60f68
Heeneth12 <heeneth123@gmail.com>
2025-12-30 19:05:40 +0530

    content changes in header and about

--- layouts/components/AboutSection.tsx
+++ layouts/components/AboutSection.tsx
@@ -2,28 +2,35 @@ export default function AboutSection() {
   return (
     <section className="mt-12">
       <h2 className="text-xl font-bold">About</h2>
-      <div className="prose  prose-zinc dark:prose-invert text-zinc-600 dark:text-zinc-400 leading-relaxed">
-        <p className="">
-          At the end of 2022, I quit my job as a software engineer to go
-          fulltime into building and scaling my own SaaS businesses. In the
-          past,
+
+      <div
+        className="
+          mt-2
+          text-zinc-600 dark:text-zinc-400
+          text-xs sm:text-base
+          font-semibold
+          leading-relaxed
+          text-left sm:text-justify
+        ">
+        <p>
+          I'm a software developer with over 2 years of experience and a
           <span className="underline decoration-zinc-400 underline-offset-4 mx-1 cursor-pointer hover:text-black dark:hover:text-white transition-colors">
-            I pursued a double degree in computer science and business
+            degree in Computer Science
           </span>
-          ,
+          . Currently building
+          <span className="underline decoration-zinc-400 underline-offset-4 mx-1 font-bold cursor-pointer hover:text-black dark:hover:text-white transition-colors">
+            EZ-Hub
+          </span>
+          , a idea with multiple interconnected SaaS products. As a solo
+          developer, I'm working on
           <span className="underline decoration-zinc-400 underline-offset-4 mx-1 cursor-pointer hover:text-black dark:hover:text-white transition-colors">
-            interned at big tech companies in Silicon Valley
+            EZ-Inventory
           </span>
-          , and
+          and other tools. I'm also
           <span className="underline decoration-zinc-400 underline-offset-4 mx-1 cursor-pointer hover:text-black dark:hover:text-white transition-colors">
-            competed in over 21 hackathons for fun
+            open to freelancing
           </span>
-          . I also had the pleasure of being a part of the first ever in-person
-          cohort of buildspace called
-          <span className="underline decoration-zinc-400 underline-offset-4 mx-1 font-semibold cursor-pointer hover:text-black dark:hover:text-white transition-colors">
-            buildspace sf1
-          </span>
-          .
+          and love building products.
         </p>
       </div>
     </section>
--- layouts/components/Header.tsx
+++ layouts/components/Header.tsx
@@ -1,25 +1,41 @@
 import Image from "next/image";
 import user from "@/assets/images/user.jpeg";
+import SplitText from "@/components/SplitText";
 
 export default function Header() {
   return (
-    <section className="flex flex-col-reverse items-start justify-between gap-8 sm:flex-row sm:items-center">
-      <div className="flex flex-col gap-4">
-        <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
-          Hi, I'm Heeneth{" "}
-          <span className="inline-block animate-wave origin-[70%_70%]">👋</span>
-        </h1>
-        <p className="max-w-md text-lg text-zinc-600 dark:text-zinc-400">
+    <section className="flex flex-row items-start justify-between gap-8 sm:items-center">
+      <div className="flex flex-col gap-2 flex-1">
+        <SplitText
+          text="Hi, I'm Heeneth 👋"
+          className="text-2xl font-bold tracking-tight sm:text-5xl text-left"
+          tag="h1"
+          delay={100}
+          duration={0.6}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+        />
+        <p
+          className="
+            max-w-md
+            text-sm sm:text-lg
+            leading-snug sm:leading-relaxed
+            text-zinc-800 dark:text-zinc-400
+          ">
           Software Engineer turned Entrepreneur. I love building things and
           helping people. Very active on Twitter.
         </p>
       </div>
 
       {/* Avatar Image */}
-      <div className="relative h-28 w-28 shrink-0 overflow-hidden rounded-full border border-zinc-200 dark:border-zinc-800">
+      <div className="relative h-18 w-18 sm:h-28 sm:w-28 shrink-0 overflow-hidden rounded-full border border-zinc-200 dark:border-zinc-800">
         <Image
           src={user}
-          alt="Dillion"
+          alt="Heeneth"
           fill
           className="object-cover"
           priority
